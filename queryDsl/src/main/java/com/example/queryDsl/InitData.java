0f9e04940008d1966ceaebc97d79985a40ae8a2f
DONGCHAN <dongchan9312@mnwise.com>
2025-01-10 13:07:42 +0900

    api 추가

--- queryDsl/src/main/java/com/example/queryDsl/InitData.java
+++ queryDsl/src/main/java/com/example/queryDsl/InitData.java
@@ -3,6 +3,7 @@ package com.example.queryDsl;
 import com.example.queryDsl.entity.Author;
 import com.example.queryDsl.entity.Book;
 import com.example.queryDsl.service.AuthorService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +15,7 @@ public class InitData {
     @Autowired
     AuthorService authorService;
 
-//    @PostConstruct
+    @PostConstruct
     public void init(){
         List<Author> all = new ArrayList<>();
         List<Book> books = new ArrayList<>();
--- queryDsl/src/main/java/com/example/queryDsl/controller/ApiController.java
+++ queryDsl/src/main/java/com/example/queryDsl/controller/ApiController.java
@@ -1,22 +1,49 @@
 package com.example.queryDsl.controller;
 
 
+import com.example.queryDsl.entity.Author;
+import com.example.queryDsl.entity.response.AuthorResponse;
 import com.example.queryDsl.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api")
+import java.util.List;
+
+@RequestMapping("/api/authors")
 @RestController
 public class ApiController {
 
     @Autowired
     AuthorService authorService;
 
-    @PostMapping("/authors")
-    public void selectAllAuthor(){
 
+    // 전체 조회
+    @GetMapping
+    public ResponseEntity<List<AuthorResponse>> getAllAuthors() {
+        List<AuthorResponse> authors = authorService.selectAllAuthor();
+        return ResponseEntity.ok(authors);
+    }
+
+    // 단건 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorResponse> getAuthorById(@PathVariable Long id) {
+        AuthorResponse author = authorService.selectAuthorByid(id);
+        return ResponseEntity.ok(author);
+    }
+
+    // 추가 또는 업데이트
+    @PostMapping
+    public ResponseEntity<Long> updateAuthorNameById(@RequestBody Author author) {
+        Long savedAuthor = authorService.updateAuthorNameById(author.getId(),author.getName());
+        return ResponseEntity.ok(savedAuthor);
+    }
+
+    // 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthorsByCondition(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
